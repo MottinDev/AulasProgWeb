2d7d9036c472e10722ee1f60c82fcfab410a0386
MottinDev <164411855+MottinDev@users.noreply.github.com>
2024-07-12 18:49:52 -0400

    Delete Aula0207 directory

--- Aula0207/src/types.ts
+++
@@ -1,9 +0,0 @@
-export interface ItemEstoque {
-    id: number;
-    nome: string;
-    quantidade: number;
-    preco: number;
-    }
-    export interface EstoqueData {
-    itens: ItemEstoque[];
-    }
